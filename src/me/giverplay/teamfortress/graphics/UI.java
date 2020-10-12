6a3b100438ed4b91bede918631fc14460e65f056
= <play.giver.007@gmail.com>
2020-10-11 21:09:55 -0300

    UI basic

--- src/me/giverplay/teamfortress/graphics/UI.java
+++ src/me/giverplay/teamfortress/graphics/UI.java
@@ -4,6 +4,7 @@ import static me.giverplay.teamfortress.game.Game.WIDTH;
 import static me.giverplay.teamfortress.game.Game.HEIGHT;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 
 import java.awt.image.BufferedImage;
@@ -39,6 +40,11 @@ public class UI
 			g.setColor(Color.WHITE);
 			g.drawRect(WIDTH - width - 10, 10, width, 12 + sprite.getHeight() * 2);
 			g.drawRect(WIDTH - width - 10, 22 + sprite.getHeight() * 2, width, height - 12 - sprite.getHeight() * 2);
+			
+			String txt = weapon.getAmmo() + "/" + player.getAmmo(weapon.getAmmoType());
+			int x = (WIDTH - 10) + ((width - g.getFontMetrics().stringWidth(txt)) / 2);
+			g.setFont(new Font("arial", Font.BOLD, 14));
+			g.drawString(txt, x, height + 8 - 10);
 		}
 	}
 }
